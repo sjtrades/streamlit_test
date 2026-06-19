5bbceb1268c8d7925384fd048750bc2f1aeecd31
Sandesh Jadhav <sandeshjtrades@gmail.com>
2026-06-19 13:31:56 +0530

    Updates

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from config import MAP_CONFIG, HEATMAP_CONFIG
 st.set_page_config(page_title="LILA BLACK - Player Journey Viewer", layout="wide")
 # st.title("LILA BLACK - Player Journey Viewer")
 
-# st.write("Version", st.__version__)
+st.write("Version", st.__version__) # Version 1.20.0
 # st.write("Has rerun", hasattr(st, "rerun"))
 # st.write("Has experimental_rerun", hasattr(st, "experimental_rerun"))
 
@@ -206,30 +206,30 @@ with right_panel:
             with c1:
                 if st.button("⏮", use_container_width=True):
                     st.session_state.playback_pct = 0
-                    # st.experimental_rerun()
-                    st.rerun()
+                    st.experimental_rerun()
+                    # st.rerun()
 
             with c2:
                 if st.button("◀", use_container_width=True):
                     st.session_state.playback_pct = max(
                         0, st.session_state.playback_pct - 1
                     )
-                    # st.experimental_rerun()
-                    st.rerun()
+                    st.experimental_rerun()
+                    # st.rerun()
 
             with c3:
                 if st.button("▶", use_container_width=True):
                     st.session_state.playback_pct = min(
                         100, st.session_state.playback_pct + 1
                     )
-                    # st.experimental_rerun()
-                    st.rerun()
+                    st.experimental_rerun()
+                    # st.rerun()
 
             with c4:
                 if st.button("⏭", use_container_width=True):
                     st.session_state.playback_pct = 100
-                    # st.experimental_rerun()
-                    st.rerun()
+                    st.experimental_rerun()
+                    # st.rerun()
 
             st.markdown("---")
             st.subheader("Match Stats")
